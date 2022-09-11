84ee2cff6e743b9edcd0062d9fbb0654d5b51e0a
Mikesew1320 <43565227+Mikesew1320@users.noreply.github.com>
2022-09-11 06:45:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ MANIFEST="git://github.com/SHRP/manifest.git -b v3_11.0"
 DEVICE=CG8
 DT_LINK="https://github.com/punkzappa007/TECNO-CG8.git -b SHRP-CG8"
 DT_PATH=device/tecno/CG8
-
+#test
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
